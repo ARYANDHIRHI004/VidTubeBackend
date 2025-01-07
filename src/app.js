84e1b753cc182b2674ca0bc2d10f61137998e440
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-01-07 16:11:13 +0530

    utils writing

--- src/app.js
+++ src/app.js
@@ -6,10 +6,13 @@ const app = express()
 
 app.use(cors(
     {
-        
+        origin:process.env.CORS_ORIGIN,
+        credentials: true
     }
 ))
-app.use(express.json())
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({limit:"16kb", extended: true}))
+app.use(express.static("public"))
 app.use(cookieParser())
 
 
