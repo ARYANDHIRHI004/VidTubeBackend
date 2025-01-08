8ce3e47635c37d4e43b33d14759d01811056ce87
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-01-08 15:33:58 +0530

    env file error solve

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 import app from "./app.js";
 
 dotenv.config({
-    path:"./.env"
+    path: "./.env"  
 })
 
 
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8000
 connectDB()
 .then((e) => {
   app.listen(PORT, (par) => {
-    console.log(`app is listening at port ${PORT}`);
+    console.log(`server is listening at port ${PORT}`);
     })
 })
 .catch((params) => {
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,35 +2,25 @@ import { v2 as cloudinary } from 'cloudinary';
 import fs from "fs"
 
 cloudinary.config({ 
-    cloud_name: "dvtyyd1rv", 
-    api_key: "853392538367718", 
-    api_secret:"uCt2_4AkqXJzTndsdd6VVmgPvg4"
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRIET
 });
-
-
 
 
 const uplodeFileToCloudinary = async(localFilePath) => {
 
     try {
-        if(!localFilePath) return null;
-
-        // console.log(localFilePath);
-        
+        if(!localFilePath) return null;        
         const response = await cloudinary.uploader.upload(localFilePath,{
             resource_type:'auto'
         })
-        console.log('file is uploaded', response.url);
         fs.unlinkSync(localFilePath)
         return response
-        
-
     } catch (error) {
         console.log('Fail in upload to cloud');
         fs.unlinkSync(localFilePath)
-
-    }
-  
+    } 
 }
 
 export { uplodeFileToCloudinary }
